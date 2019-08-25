14f3b71e03ca42220acf4f96398cd7d5d2cd9573
Maus <51877347+MausPotato@users.noreply.github.com>
2019-08-26 01:17:05 +0800

    OOPS! RANDOM!

--- js/game.js
+++ js/game.js
@@ -6,8 +6,15 @@ const SuitEnum = {
 }
 
 const ColorEnum = {
-  BLACK: 0,
-  RED: 1
+  BLACK: 'black',
+  RED: 'red'
+}
+
+const PileEnum = {
+  CELL: 0,
+  FOUNDATION: 1,
+  TABLEAU: 2,
+  HAND: 3
 }
 
 class Card {
@@ -63,6 +70,15 @@ class Card {
   valueOf() {
     return this.point;
   }
+  static isSameSuit(card1, card2) {
+    return card1.suit == card2.suit;
+  }
+  static isSameColor(card1, card2) {
+    return card1.color == card2.color;
+  }
+  print() {
+    console.log('%c'+ this.toString(), 'color:' + this.color); 
+  }
 }
 
 class Deck {
@@ -83,5 +99,54 @@ class Deck {
   }
   place(deck) {
     this.cards.concat(deck.cards);
+  }
+  shuffle() {
+    let length = this.cards.length;
+    let random;
+    while (length) {
+      random = Math.floor(Math.random() * length--);
+      [this.cards[length], this.cards[random]] = [this.cards[random], this.cards[length]];
+    }
+  }
+  toString() {
+    let string = '['
+    for (let c of this.cards) {
+      string += c.toString();
+    }
+    return string + ']';
+  }
+  print() {
+    let string = '', css = [];
+    for (let c of this.cards) {
+      string += '%c' + c.toString();
+      css.push('color:' + c.color);
+    }
+    console.log(string, ...css);
+  }
+  static fullSet() {
+    let deck = new Deck([], 52);
+    for (let suit in SuitEnum) {
+      for (let point = 1; point <= 13; point++) {
+        deck.push(new Card(SuitEnum[suit], point));
+      }
+    }
+    return deck;
+  }
+}
+
+class FreeCell {
+  constructor() {
+    this.decks = [[], [], [], []];
+    this.initialize();
+  }
+  initialize() {
+    for (let i = 0; i < 4; i++) {
+      this.decks[PileEnum.CELL].push(new Deck([], 1));
+      this.decks[PileEnum.FOUNDATION].push(new Deck([], 13));
+    }
+    for (let i = 0; i < 8; i++) {
+      this.decks[PileEnum.TABLEAU].push(new Deck());
+    }
+    this.decks[PileEnum.HAND].push(new Deck());
   }
 }
